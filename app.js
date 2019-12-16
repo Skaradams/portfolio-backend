c67eaa4fe7c18a9eef0a8809bcdcae44d1b6b8e1
Damien Corticchiato <skaradams69@gmail.com>
2019-12-16 16:55:41 +0100

    emails should be sent

--- app.js
+++ app.js
@@ -16,31 +16,32 @@ app.get('/test', (req, res) => {
   res.end()
 })
 app.post('/send_email', function (req, res) {
-  const { CONTACT_EMAIL, CONTACT_PASSWORD } = process.env;
-  console.log(CONTACT_EMAIL, CONTACT_PASSWORD);
-  // let transporter = nodeMailer.createTransport({
-  //   host: 'smtp.gmail.com',
-  //   port: 465,
-  //   secure: true,
-  //   auth: {
-  //       // should be replaced with real sender's account
-  //       user: 'hello@gmail.com',
-  //       pass: 'test'
-  //   }
-  // });
-  // let mailOptions = {
-  //   // should be replaced with real recipient's account
-  //   to: 'info@gmail.com',
-  //   subject: req.body.subject,
-  //   text: req.body.message
-  // };
-  // transporter.sendMail(mailOptions, (error, info) => {
-  //   if (error) {
-  //       return console.log(error);
-  //   }
-  //   console.log('Message %s sent: %s', info.messageId, info.response);
-  // });
-  // res.writeHead(301, { Location: 'index.html' });
+  const { SENDER_EMAIL, SENDER_PASSWORD, RECIPIENT_EMAIL } = process.env;
+
+  let transporter = nodeMailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    
+    auth: {
+        // should be replaced with real sender's account
+        user: SENDER_EMAIL,
+        pass: SENDER_PASSWORD
+    }
+  });
+  let mailOptions = {
+    // should be replaced with real recipient's account
+    to: RECIPIENT_EMAIL,
+    subject: req.body.subject,
+    text: req.body.message
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+  });
+  res.writeHead(301, { Location: 'index.html' });
   res.end();
 });
 
